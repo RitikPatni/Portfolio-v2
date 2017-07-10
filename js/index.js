3138ecb98a3c7952d8f6bd3a6db5f0ecd7346d5c
Ritik <patniritik@gmail.com>
2017-07-10 19:30:36 +0530

    Analytics clicky removed

--- js/index.js
+++ js/index.js
@@ -16,23 +16,3 @@ $(document)
     a.src = g;
     m.parentNode.insertBefore(a, m)
 })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-ga('create', 'UA-101094038-1', 'auto');
-ga('send', 'pageview');
-(function(add, cla) {
-    window['UserHeatTag'] = cla;
-    window[cla] = window[cla] || function() {
-        (window[cla].q = window[cla].q || []).push(arguments)
-    }, window[cla].l = 1 * new Date();
-    var ul = document.createElement('script');
-    var tag = document.getElementsByTagName('script')[0];
-    ul.async = 1;
-    ul.src = add;
-    tag.parentNode.insertBefore(ul, tag);
-})('//uh.nakanohito.jp/uhj2/uh.js', '_uhtracker');
-_uhtracker({
-    id: 'uhjTQLNGne'
-});
-try {
-    clicky.init(101057759);
-} catch (e) {}
