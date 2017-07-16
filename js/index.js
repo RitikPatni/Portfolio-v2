7690ef77df2a7cb9a60ce8e7767352181e54d0d8
Ritik <patniritik@gmail.com>
2017-07-16 15:21:43 +0530

    Mobile contact Me Buttons fixed

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
-$(document)
-    .ready(function () {
-        $(".button-collapse")
-            .sideNav({ closeOnClick: true, draggable: true });
-        $('.scrollspy')
-            .scrollSpy({ scrollOffset: 30 });
-    });
-(function (i, s, o, g, r, a, m) {
-i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-    (i[r].q = i[r].q || []).push(arguments)
-}, i[r].l = 1 * new Date(); a = s.createElement(o),
-    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+$(document).ready(function() {
+    $(".button-collapse")
+        .sideNav({ closeOnClick: true, draggable: true });
+    $('.scrollspy')
+        .scrollSpy({ scrollOffset: 30 });
+    if ($(window).width() < 500) {
+        $('.btn').removeClass('btn');
+    }
+});
+
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
 })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-101094038-1', 'auto');
